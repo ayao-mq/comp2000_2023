2558d291f4221132176dfc674d9522a62e3695c8
ayao-mq <andrew.yao@students.mq.edu.au>
2023-08-17 16:10:02 +1000

    Week 4 complete

--- src/Actor.java
+++ src/Actor.java
@@ -3,21 +3,3 @@ import java.awt.Graphics;
 import java.awt.Polygon;
 import java.util.ArrayList;
 
-public abstract class Actor {
-  Color color;
-  ArrayList<Polygon> polygons;
-  Cell loc;
-
-  public void paint(Graphics g) {
-    // g.setColor(color);
-    // g.fillRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-    // g.setColor(Color.GRAY);
-    // g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
-    for (Polygon polygon : polygons) {
-      g.setColor(color);
-      g.fillPolygon(polygon);
-      g.setColor(Color.GRAY);
-      g.drawPolygon(polygon);
-    }
-  }
-}
